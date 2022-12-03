3142510631268c59c04b10959428d3e6dbb1e2ec
Máté Szűcs <szcsm2000@gmail.com>
2022-12-03 14:48:46 +0100

    Add files via upload

--- day01/day01.py
+++ day01/day01.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 
-inFile = "day01/input.txt"
+inFile = 'day01/input.txt'
 
 def partOne(inFile: str) -> int:
     with open(inFile) as f:
         maxval = 0                                          # max sum of calories from elves
         currval = 0                                         # current count of calories from current elf
-        for line in f.readlines():                          # go through each value in the input txt line by line
+        for line in f:                                      # go through each value in the input txt line by line
             if line == "\n":                                # if it's a blank line then check current count to max value
                 if currval > maxval:                        # if larger, make it max value and reset current count
                     maxval = currval
@@ -20,14 +20,14 @@ def partOne(inFile: str) -> int:
 
     return maxval
 
-print("Solution of part one: ", partOne(inFile))
+print('Solution of part one: ', partOne(inFile))
 
 def partTwo(inFile: str) -> int:                            # basically a more general approach to part one
     counts = []                                             # sum up individual elves' calories as before, but store all values
     with open(inFile) as f:                                 # in a list
         currval = 0
-        for idx, val in enumerate(f.readlines()):
-            if val == "\n":
+        for idx, val in enumerate(f):
+            if val == '\n':
                 counts.append(currval)
                 currval = 0
             else:
@@ -35,4 +35,4 @@ def partTwo(inFile: str) -> int:                            # basically a more g
 
     return np.sum(sorted(counts)[::-1][:3])                 # sort the list and return sum of the three largest values
 
-print("Solution of part two: ", partTwo(inFile))
+print('Solution of part two: ', partTwo(inFile))
--- day02/day02.py
+++ day02/day02.py
@@ -1,68 +1,44 @@
 import numpy as np
 
 
-inFile = "day02/input.txt"
+inFile = 'day02/input.txt'
 
-def partOne(inFile: str) -> int:
+def partOne(inFile: str) -> int:                            
     count = 0
+    values = {                                                  # we store all the possible outcomes in a dict
+        ('A', 'X'): 4,
+        ('A', 'Y'): 8,
+        ('A', 'Z'): 3,
+        ('B', 'X'): 1,
+        ('B', 'Y'): 5,
+        ('B', 'Z'): 9,
+        ('C', 'X'): 7,
+        ('C', 'Y'): 2,
+        ('C', 'Z'): 6
+    }
     with open(inFile) as f:
-        for line in f.readlines():
-            if line[0] == 'A':
-                if line[2] == 'X':
-                    count += 4
-                elif line[2] == 'Y':
-                    count += 8
-                else:
-                    count += 3
-            elif line[0] == 'B':
-                if line[2] == 'X':
-                    count += 1
-                elif line[2] == 'Y':
-                    count += 5
-                else:
-                    count += 9
-            elif line[0] == 'C':
-                if line[2] == 'X':
-                    count += 7 
-                elif line[2] == 'Y':
-                    count += 2
-                else:
-                    count += 6
-            else:
-                pass
-
+        for line in f:
+            count += values[(line[0], line[2])]                # then simply add the corresponding value to the count 
     return count
 
-print("Solution of part one: ", partOne(inFile))
+print('Solution of part one: ', partOne(inFile))
 
 def partTwo(inFile: str) -> int:
     count = 0
+    values = {                                                 # same as before, but with diffrent values
+        ('A', 'X'): 3,
+        ('A', 'Y'): 4,
+        ('A', 'Z'): 8,
+        ('B', 'X'): 1,
+        ('B', 'Y'): 5,
+        ('B', 'Z'): 9,
+        ('C', 'X'): 2,
+        ('C', 'Y'): 6,
+        ('C', 'Z'): 7
+    }
     with open(inFile) as f:
-        for line in f.readlines():
-            if line[0] == 'A':
-                if line[2] == 'X':
-                    count += 3
-                elif line[2] == 'Y':
-                    count += 4
-                else:
-                    count += 8
-            elif line[0] == 'B':
-                if line[2] == 'X':
-                    count += 1
-                elif line[2] == 'Y':
-                    count += 5
-                else:
-                    count += 9
-            elif line[0] == 'C':
-                if line[2] == 'X':
-                    count += 2 
-                elif line[2] == 'Y':
-                    count += 6
-                else:
-                    count += 7
-            else:
-                pass
-
+        for line in f:
+            count += values[(line[0], line[2])]
     return count
 
-print("Solution of part two: ", partTwo(inFile))
+print('Solution of part two: ', partTwo(inFile))
